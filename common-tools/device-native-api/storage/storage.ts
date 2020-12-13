45ed8f8ddacc71eaecf6bfb4b419b795430d1c9c
Fernando Medina <fermmm@gmail.com>
2020-12-13 01:46:18 -0300

    Updated local storage library

--- common-tools/device-native-api/storage/storage.ts
+++ common-tools/device-native-api/storage/storage.ts
@@ -1,21 +1,30 @@
 import * as SecureStore from "expo-secure-store";
-import { AsyncStorage } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export async function saveOnDeviceSecure(key: string, value: string): Promise<void> {
-   const secureStorageAvaiable: boolean = await isSecureStorageAvaiable();
-   if (secureStorageAvaiable) {
+   const secureStorageAvailable: boolean = await isSecureStorageAvailable();
+   if (secureStorageAvailable) {
       return SecureStore.setItemAsync(key, value);
    } else {
-      return AsyncStorage.setItem(key, value);
+      return saveOnDevice(key, value);
    }
 }
 
 export async function loadFromDeviceSecure(key: string): Promise<string> {
-   const secureStorageAvaiable: boolean = await isSecureStorageAvaiable();
-   if (secureStorageAvaiable) {
+   const secureStorageAvailable: boolean = await isSecureStorageAvailable();
+   if (secureStorageAvailable) {
       return SecureStore.getItemAsync(key);
    } else {
-      return AsyncStorage.getItem(key);
+      return loadFromDevice(key);
+   }
+}
+
+export async function removeFromDeviceSecure(key: string): Promise<void> {
+   const secureStorageAvailable: boolean = await isSecureStorageAvailable();
+   if (secureStorageAvailable) {
+      return SecureStore.deleteItemAsync(key);
+   } else {
+      return removeFromDevice(key);
    }
 }
 
@@ -27,7 +36,11 @@ export async function loadFromDevice(key: string): Promise<string> {
    return AsyncStorage.getItem(key);
 }
 
-async function isSecureStorageAvaiable(): Promise<boolean> {
+export async function removeFromDevice(key: string): Promise<void> {
+   return AsyncStorage.removeItem(key);
+}
+
+async function isSecureStorageAvailable(): Promise<boolean> {
    await SecureStore.setItemAsync("test", "1234");
-   return Promise.resolve(await SecureStore.getItemAsync("test") === "1234");
+   return Promise.resolve((await SecureStore.getItemAsync("test")) === "1234");
 }
