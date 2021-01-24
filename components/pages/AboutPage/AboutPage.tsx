709d23b862d328cea297401f01c733e7d69eb918
Fernando Medina <fermmm@gmail.com>
2021-01-23 22:28:13 -0300

    Improved obout section texts

--- components/pages/AboutPage/AboutPage.tsx
+++ components/pages/AboutPage/AboutPage.tsx
@@ -18,9 +18,13 @@ const AboutPage: FC = () => {
             <Text>
                El mínimo de personas necesario para que se genere una cita son 3 personas y 4 para
                grupos que sean 100% heterosexuales. El máximo es de 20. Las citas tradicionales de 2
-               personas no son posibles en esta app. Tu primera cita grupal es probable que sea
-               entre pocos usuarios y las siguientes con más, ya que se necesita mas tiempo para que
-               se den coincidencias mas numerosas.
+               personas no son posibles en esta app.
+            </Text>
+            <EmptySpace height={15} />
+            <Text>
+               Tu primera cita grupal es probable que sea entre pocos usuarios, por ejemplo de 3
+               personas o tal vez 6, pero las siguientes serán de más, ya que se necesita mas tiempo
+               para que se den coincidencias mas numerosas.
             </Text>
             <TitleText extraSize style={styles.title}>
                ¿Si solo me gusta un género, significa que en la cita grupal van a ser todxs de ese
@@ -28,18 +32,18 @@ const AboutPage: FC = () => {
             </TitleText>
             <Text>
                No. Para estar en una cita grupal te tienes que gustar con un mínimo de 2 de sus
-               integrantes y no necesariamente con la totalidad, asi que es muy probable que haya
+               integrantes, no necesariamente con la totalidad, por lo que es muy probable que haya
                algunas personas en tus citas con quienes no te gustes, que pueden ser de cualquier
                género y sexualidad.
             </Text>
             <TitleText extraSize style={styles.title}>
                ¿Se pueden generar citas desequilibradas? (Ej: 1 persona se gusta con 5 que no se
-               gustan entre ellxs)
+               gustan entre ellas)
             </TitleText>
             <Text>
                No. Un requisito para que se forme una cita grupal es que sean grupos mas o menos
-               equilibrados. Esto lleva a que por ejemplo en un grupo 100% heterosexual la cantidad
-               de mujeres y hombres siempre va a ser mas o menos la misma.
+               equilibrados. Esto también genera que en las citas 100% heterosexuales la cantidad de
+               mujeres y hombres siempre va a ser mas o menos la misma.
             </Text>
             <TitleText extraSize style={styles.title}>
                ¿Es fácil que se genere una cita grupal?
@@ -51,11 +55,12 @@ const AboutPage: FC = () => {
             </Text>
             <EmptySpace height={15} />
             <Text>
-               En las "citas de monogamia" uno puede pasar varias horas con una única persona por lo
-               que esta tiene que tener una buena cantidad de características o coincidencias
-               buscadas, esto hace escasear los likes, en cambio en una cita grupal nadie tiene que
-               "tenerlo todo", por lo que en esta app se tiende a ser mas flexible al dar likes y
-               por eso también aumentan las posibilidades de una coincidencia grupal.
+               En las "citas de monogamia" unx pasa el tiempo con otra persona durante al menos
+               muchas horas, por lo que la persona buscada debería reunir todas las características
+               deseadas en una sola, ya que no va a haber otra. Esto hace escasear los likes en esas
+               apps, en una cita grupal en cambio, nadie tiene que "tenerlo todo en uno", por lo que
+               en esta app se tiende a ser mas flexible al dar likes y por eso también aumentan las
+               posibilidades de una coincidencia grupal.
             </Text>
             <EmptySpace height={15} />
             <Text>
@@ -90,50 +95,30 @@ const AboutPage: FC = () => {
                hasta otrxs ingenieros de software.
             </Text>
             <TitleText extraSize style={styles.title}>
-               ¿No es demasiado rara esta aplicación?
+               ¿Tienen sentido las citas grupales?
             </TitleText>
             <Text>
-               Relacionarnos como en pareja pero en grupo es a menudo una experiencia
-               sorprendente-mente fluida y cómoda, muy presente en la historia de la sexualidad. Hay
-               grandes libros de divulgación científica sobre el tema para recomendar como los de
-               "Christopher Ryan" en primer lugar o algunos de "Friedrich Engels" como "El origen de
-               la familia, la propiedad privada y el estado". Aquí vamos con un resumen histórico
-               que puede resultarles interesante y util para saber más sobre sexualidad y comprender
-               lo interesante de este tipo de proyectos:
+               El afecto puede tomar cualquier forma, pero si hablamos de sexualidad, el sexo grupal
+               es a menudo una experiencia sorprendente-mente cómoda y fluida, se siente normal a
+               pesar de que no lo sea en nuestra cultura. Sin embargo esta muy presente en la
+               historia de la sexualidad y en nuestro cuerpo, por ejemplo: Los "gemidos sexuales"
+               son un instinto que las otras especies que también lo tienen (primates) lo usan para
+               llamar a los que están cerca, de la misma manera funcionaba originalmente en nuestra
+               especie, la sexualidad que podemos experimentar en nuestra cultura esta muy alejada
+               del tipo de sexualidad que nos pide nuestro cuerpo y su evolución. Hay grandes libros
+               de divulgación científica sobre el tema para recomendar, algunos muy conocidos como
+               los de Christopher Ryan en primer lugar o también el de Friedrich Engels: "El origen
+               de la familia, la propiedad privada y el estado".
             </Text>
             <EmptySpace height={15} />
             <Text>
-               El ser humano primitivo inteligente (el cazador recolector) vivía en tribus donde se
-               compartía la comida, la crianza de lxs hijxs y el sexo. Lxs padres de lxs hijxs eran
-               todxs, el sexo y el afecto eran de tipo no monógamo y/o grupal, la comida todxs
-               sabían obtenerla, por lo que no se recurría a nadie en particular para sobrevivir si
-               no a toda la tribu, debido a esto último no podían producirse estructuras de poder
-               verticales y violentas como las de hoy en día: patriarcado, explotación, etc.
-            </Text>
-            <EmptySpace height={15} />
-            <Text>
-               Más para atrás en la historia, durante nuestra evolución tuvimos esta misma forma de
-               vida. Esto es importante por que la sexualidad de cada persona esta influenciada en
-               gran medida por su cuerpo y su cuerpo por la evolución. Nuestra cultura nos impone
-               una "sexualidad cultural" pero también tenemos una "sexualidad física". Por ejemplo:
-               El porno, extremadamente popular, donde se busca siempre contenido nuevo con personas
-               nuevas, donde muchas personas a la vez participando en una misma escena es algo
-               normal, donde nadie elige ver a una persona preferida y abandonar por años el resto
-               del contenido, muestra que cuando somos libres de elegir y nadie nos ve, preferimos
-               nuestra sexualidad física. Los "gemidos sexuales" aparecen en nuestro instinto para
-               llamar a los que están cerca a que se unan a la experiencia y se convierta en una
-               experiencia grupal, este instinto y sus efectos fueron observados en varias especies
-               ademas del humano, es otro ejemplo de la sexualidad física.
-            </Text>
-            <EmptySpace height={15} />
-            <Text>
-               Cuando se instala la monogamia, no sucede por que esta tenga alguna ventaja emocional
-               o por ser mas profunda en las relaciones afectivas o sexuales, al contrario, sucede
-               por cuestiones comerciales: casar hijxs como contrato comercial entre padres,
-               acumular riqueza de forma personal sin compartir con la tribu, etc. La monogamia es
-               uno de los muchos fenómenos de la revolución neolítica, la que trajo la imagen del
-               ser humano primitivo que era violento y patriarcal, la imagen más difundida pero la
-               que menos tiempo ocupa en nuestra historia y evolución.
+               Cuando se instala la monogamia en nuestra cultura, no sucede por que esta tenga mas
+               sentido, alguna ventaja emocional, ni por ser mas profunda o duradera en las
+               relaciones afectivas o sexuales, sucede por cuestiones comerciales: casar hijxs como
+               contrato comercial entre familias, acumular riqueza de forma personal sin compartir
+               con la tribu, etc. La monogamia y la familia nuclear son dos de los muchos fenómenos
+               de la revolución neolítica, la misma que trajo por primera vez a las clases sociales,
+               patriarcado y otras opresiones de la humanidad.
             </Text>
             <EmptySpace height={80} />
          </BasicScreenContainer>
