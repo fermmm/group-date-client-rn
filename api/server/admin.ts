1cd81a5619aa000878cab67265f9c7a427b65a10
Fernando Medina <fermmm@gmail.com>
2021-02-01 16:33:36 -0300

    Changed create fake users route

--- api/server/admin.ts
+++ api/server/admin.ts
@@ -6,7 +6,11 @@ export async function sendCreateFakeUsers(
    params: TokenParameter & { text: string },
    autoRevalidateRelated: boolean = true
 ) {
-   const resp = await defaultHttpRequest<typeof params, string>("createFakeUsers", "GET", params);
+   const resp = await defaultHttpRequest<typeof params, string>(
+      "testing/create-fake-users",
+      "GET",
+      params
+   );
    if (autoRevalidateRelated) {
       revalidate("cards-game/recommendations");
    }
