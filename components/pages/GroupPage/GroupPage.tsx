9f7d4aa8106ce8e68b802e5aa52340cdd37b25b8
Fernando Medina <fermmm@gmail.com>
2021-02-03 01:54:59 -0300

    Ported GroupListPage and GroupPage to hooks

--- components/pages/GroupPage/GroupPage.tsx
+++ components/pages/GroupPage/GroupPage.tsx
@@ -1,10 +1,7 @@
-import React, { Component } from "react";
+import React, { FC, useState } from "react";
 import { StyleSheet, View } from "react-native";
-import { Route } from "@react-navigation/native";
-import { withTheme, List } from "react-native-paper";
-import { StackNavigationProp, StackScreenProps } from "@react-navigation/stack";
-import { withNavigation } from "@react-navigation/compat";
-import { Themed, ThemeExt } from "../../../common-tools/themes/types/Themed";
+import { useRoute } from "@react-navigation/native";
+import { List } from "react-native-paper";
 import { Styles } from "../../../common-tools/ts-tools/Styles";
 import AppBarHeader from "../../common/AppBarHeader/AppBarHeader";
 import Avatar from "../../common/Avatar/Avatar";
@@ -18,104 +15,93 @@ import CardAcceptInvitation from "./CardAcceptInvitation/CardAcceptInvitation";
 import BadgeExtended from "../../common/BadgeExtended/BadgeExtended";
 import { Group } from "../../../api/server/shared-tools/endpoints-interfaces/groups";
 import { User } from "../../../api/server/shared-tools/endpoints-interfaces/user";
+import { useNavigation } from "../../../common-tools/navigation/useNavigation";
+import { RouteProps } from "../../../common-tools/ts-tools/router-tools";
 
-export interface GroupPageProps extends Themed, StackScreenProps<{}> {}
-export interface GroupPageState {
-   expandedUser: number;
+export interface ParamsGroupPage {
+   group: Group;
 }
 
-class GroupPage extends Component<GroupPageProps, GroupPageState> {
-   state: GroupPageState = {
-      expandedUser: -1
-   };
+const GroupPage: FC = () => {
+   const { navigate } = useNavigation();
+   const [expandedUser, setExpandedUser] = useState(-1);
+   const { params } = useRoute<RouteProps<ParamsGroupPage>>();
+   const group: Group = params?.group;
 
-   render(): JSX.Element {
-      const { expandedUser }: Partial<GroupPageState> = this.state;
-      const { colors }: ThemeExt = (this.props.theme as unknown) as ThemeExt;
-      const { navigate }: StackNavigationProp<Record<string, {}>> = this.props.navigation;
-      const route: Route<string, { group: Group }> = this.props.route as Route<
-         string,
-         { group: Group }
-      >;
-      const group: Group = route.params.group;
+   // TODO: Remove this
+   const invitationAccepted: boolean = true;
 
-      // TODO: Remove this
-      const invitationAccepted: boolean = true;
-
-      return (
-         <>
-            <AppBarHeader title={invitationAccepted ? "Grupo" : "Invitación a una cita"}>
-               {invitationAccepted && (
-                  <View>
-                     <ButtonForAppBar icon="forum" onPress={() => navigate("Chat")}>
-                        Chat grupal
-                     </ButtonForAppBar>
-                     <BadgeExtended showAtLeftSide>3</BadgeExtended>
-                  </View>
-               )}
-            </AppBarHeader>
-            <BasicScreenContainer>
-               {!invitationAccepted && (
-                  <CardAcceptInvitation
-                     matchAmount={3}
-                     onAcceptPress={() => navigate("DateVoting", { group })}
-                  />
-               )}
-               {invitationAccepted && (
-                  <CardDateInfo onModifyVotePress={() => navigate("DateVoting", { group })} />
-               )}
-               <SurfaceStyled>
-                  <TitleText>Miembros del grupo:</TitleText>
-                  <List.Section>
-                     {group.members.map((user, i) => (
-                        <List.Accordion
-                           title={user.name}
-                           expanded={i === expandedUser}
-                           onPress={() =>
-                              this.setState({ expandedUser: expandedUser !== i ? i : -1 })
-                           }
-                           titleStyle={styles.itemTitle}
-                           left={props => (
-                              <Avatar {...props} size={50} source={{ uri: user.images[0] }} />
-                           )}
-                           key={i}
-                        >
-                           <List.Section
-                              title="Se gusta con:"
-                              style={styles.subItemTitle}
-                              titleStyle={styles.sectionTitle}
-                           >
-                              {this.getMatchesOf(user.userId, group).map((matchedUser, u) => (
-                                 <List.Item
-                                    title={matchedUser.name}
-                                    style={styles.subItem}
-                                    key={u}
-                                    onPress={() => navigate("Profile", { user: matchedUser })}
-                                    left={props => (
-                                       <Avatar
-                                          {...props}
-                                          onPress={() => navigate("Profile", { user: matchedUser })}
-                                          size={50}
-                                          source={{ uri: matchedUser.images[0] }}
-                                       />
-                                    )}
-                                 />
-                              ))}
-                           </List.Section>
-                        </List.Accordion>
-                     ))}
-                  </List.Section>
-               </SurfaceStyled>
-            </BasicScreenContainer>
-         </>
-      );
-   }
-
-   getMatchesOf(userId: string, group: Group): User[] {
+   const getMatchesOf = (userId: string, group: Group): User[] => {
       const matchesList: string[] = group.matches.find(m => m.userId === userId).matches;
       return group.members.filter(u => matchesList.includes(u.userId));
-   }
-}
+   };
+
+   return (
+      <>
+         <AppBarHeader title={invitationAccepted ? "Grupo" : "Invitación a una cita"}>
+            {invitationAccepted && (
+               <View>
+                  <ButtonForAppBar icon="forum" onPress={() => navigate("Chat")}>
+                     Chat grupal
+                  </ButtonForAppBar>
+                  <BadgeExtended amount={3} showAtLeftSide />
+               </View>
+            )}
+         </AppBarHeader>
+         <BasicScreenContainer>
+            {!invitationAccepted && (
+               <CardAcceptInvitation
+                  matchAmount={3}
+                  onAcceptPress={() => navigate("DateVoting", { group })}
+               />
+            )}
+            {invitationAccepted && (
+               <CardDateInfo onModifyVotePress={() => navigate("DateVoting", { group })} />
+            )}
+            <SurfaceStyled>
+               <TitleText>Miembros del grupo:</TitleText>
+               <List.Section>
+                  {group.members.map((user, i) => (
+                     <List.Accordion
+                        title={user.name}
+                        expanded={i === expandedUser}
+                        onPress={() => setExpandedUser(expandedUser !== i ? i : -1)}
+                        titleStyle={styles.itemTitle}
+                        left={props => (
+                           <Avatar {...props} size={50} source={{ uri: user.images[0] }} />
+                        )}
+                        key={i}
+                     >
+                        <List.Section
+                           title="Se gusta con:"
+                           style={styles.subItemTitle}
+                           titleStyle={styles.sectionTitle}
+                        >
+                           {getMatchesOf(user.userId, group).map((matchedUser, u) => (
+                              <List.Item
+                                 title={matchedUser.name}
+                                 style={styles.subItem}
+                                 key={u}
+                                 onPress={() => navigate("Profile", { user: matchedUser })}
+                                 left={props => (
+                                    <Avatar
+                                       {...props}
+                                       onPress={() => navigate("Profile", { user: matchedUser })}
+                                       size={50}
+                                       source={{ uri: matchedUser.images[0] }}
+                                    />
+                                 )}
+                              />
+                           ))}
+                        </List.Section>
+                     </List.Accordion>
+                  ))}
+               </List.Section>
+            </SurfaceStyled>
+         </BasicScreenContainer>
+      </>
+   );
+};
 
 const styles: Styles = StyleSheet.create({
    itemTitle: {
@@ -133,6 +119,4 @@ const styles: Styles = StyleSheet.create({
    }
 });
 
-// tslint:disable-next-line: ban-ts-ignore-except-imports
-// @ts-ignore
-export default withTheme(withNavigation(GroupPage));
+export default GroupPage;
--- components/pages/GroupsListPage/GroupsListPage.tsx
+++ components/pages/GroupsListPage/GroupsListPage.tsx
@@ -1,70 +1,57 @@
-import React, { Component } from "react";
+import React, { FC } from "react";
 import { StyleSheet } from "react-native";
-import { withNavigation } from "@react-navigation/compat";
-import { StackNavigationProp, StackScreenProps } from "@react-navigation/stack";
 import { Styles } from "../../../common-tools/ts-tools/Styles";
-import { withTheme, List } from "react-native-paper";
+import { List } from "react-native-paper";
 import GraphSvg2 from "../../../assets/GraphSvg2";
-import { ThemeExt, Themed } from "../../../common-tools/themes/types/Themed";
-import { Group } from "../../../api/server/shared-tools/endpoints-interfaces/groups";
 import BasicScreenContainer from "../../common/BasicScreenContainer/BasicScreenContainer";
 import TitleText from "../../common/TitleText/TitleText";
 import EmptyPageMessage from "../../common/EmptyPageMessage/EmptyPageMessage";
+import { useUserGroupList } from "../../../api/server/groups";
+import { useNavigation } from "../../../common-tools/navigation/useNavigation";
+import { toFirstUpperCase } from "../../../common-tools/js-tools/js-tools";
 
-export interface GroupsListPageProps extends Themed, StackScreenProps<{}> {}
-export interface GroupsListPageState {
-   groups: Group[];
-}
+const GroupsListPage: FC = () => {
+   const { data: groups } = useUserGroupList();
+   const { navigate } = useNavigation();
 
-class GroupsListPage extends Component<GroupsListPageProps, GroupsListPageState> {
-   state: GroupsListPageState = {
-      groups: [] // Uncomment this line to test the "no groups" UI
-      // TODO: Get groups from server
-      // groups: getGroups()
-   };
-
-   render(): JSX.Element {
-      const { colors }: ThemeExt = (this.props.theme as unknown) as ThemeExt;
-      const { navigate }: StackNavigationProp<Record<string, {}>> = this.props.navigation;
-      const { groups }: Partial<GroupsListPageState> = this.state;
-
-      if (groups.length === 0) {
-         return (
-            <EmptyPageMessage
-               text={
-                  "Acá van a aparecer tus citas grupales. Las citas se forman cuando se gustan varias personas formando un grupo."
-               }
-            />
-         );
-      }
-
+   if (groups == null || groups?.length === 0) {
       return (
-         <BasicScreenContainer>
-            <TitleText extraMarginLeft extraSize>
-               Tus citas grupales
-            </TitleText>
-            <List.Section>
-               {groups.map((group, i) => (
-                  <List.Item
-                     title={group.members.map((user, u) => (u > 0 ? ", " : "") + user.name)}
-                     description="Cita dentro de 2 días"
-                     left={props => (
-                        <List.Icon
-                           {...props}
-                           icon={({ color: c }) => (
-                              <GraphSvg2 circleColor={c} lineColor={c} style={styles.logo} />
-                           )}
-                        />
-                     )}
-                     onPress={() => navigate("Group", { group })}
-                     key={i}
-                  />
-               ))}
-            </List.Section>
-         </BasicScreenContainer>
+         <EmptyPageMessage
+            text={
+               "Aquí van a aparecer tus citas grupales. Las citas se forman cuando se gustan varias personas formando un grupo."
+            }
+         />
       );
    }
-}
+
+   return (
+      <BasicScreenContainer>
+         <TitleText extraMarginLeft extraSize>
+            Tus citas grupales
+         </TitleText>
+         <List.Section>
+            {groups.map((group, i) => (
+               <List.Item
+                  title={group.members.map(
+                     (user, u) => (u > 0 ? ", " : "") + toFirstUpperCase(user.name)
+                  )}
+                  description={`Cita de ${group.members.length} personas`}
+                  left={props => (
+                     <List.Icon
+                        {...props}
+                        icon={({ color: c }) => (
+                           <GraphSvg2 circleColor={c} lineColor={c} style={styles.logo} />
+                        )}
+                     />
+                  )}
+                  onPress={() => navigate("Group", { group })}
+                  key={i}
+               />
+            ))}
+         </List.Section>
+      </BasicScreenContainer>
+   );
+};
 
 const styles: Styles = StyleSheet.create({
    scene: {
@@ -72,6 +59,4 @@ const styles: Styles = StyleSheet.create({
    }
 });
 
-// tslint:disable-next-line: ban-ts-ignore-except-imports
-// @ts-ignore
-export default withTheme(withNavigation(GroupsListPage));
+export default GroupsListPage;
