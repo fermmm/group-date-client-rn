62cde2a7d2256b3393044778b89d9c280eb88f24
Fernando Medina <fermmm@gmail.com>
2021-02-24 01:22:40 -0300

    Improved about page texts

--- components/pages/AboutPage/AboutPage.tsx
+++ components/pages/AboutPage/AboutPage.tsx
@@ -14,11 +14,10 @@ const AboutPage: FC = () => {
          <AppBarHeader />
          <BasicScreenContainer style={styles.mainContainer}>
             <TitleText extraSize style={styles.title}>
-               ¿De cuántas personas son las citas?
+               ¿De cuántas personas son las citas grupales de la app?
             </TitleText>
             <Text style={styles.text}>
-               El mínimo de personas necesario para que se genere una cita son 3 personas y 4 para
-               grupos que sean 100% heterosexuales. El máximo es de 20.
+               El mínimo de personas necesario son 3, el máximo es de 20.
             </Text>
             <EmptySpace height={15} />
             <Text style={styles.text}>
@@ -29,24 +28,6 @@ const AboutPage: FC = () => {
                Tu primera cita grupal es probable que sea pequeña, por ejemplo de 3 personas o tal
                vez 6, es probable que las siguientes sean de más, ya que se necesita tiempo para que
                se den coincidencias más numerosas.
-            </Text>
-            <TitleText extraSize style={styles.title}>
-               ¿Si solo me gusta un género, significa que en la cita grupal van a ser todxs de ese
-               género?
-            </TitleText>
-            <Text style={styles.text}>
-               No. Para estar en una cita grupal te tienes que gustar con un mínimo de 2 de sus
-               integrantes, no necesariamente con la totalidad, es muy probable que haya personas en
-               tus citas que pueden ser de cualquier género y sexualidad.
-            </Text>
-            <TitleText extraSize style={styles.title}>
-               ¿Se pueden generar citas desequilibradas? (Ej: 1 persona se gusta con 5 que no se
-               gustan entre ellas)
-            </TitleText>
-            <Text style={styles.text}>
-               No. Un requisito para que se forme una cita grupal es que sean grupos más o menos
-               equilibrados. Esto también genera que en las citas 100% heterosexuales la cantidad de
-               mujeres y hombres siempre va a ser mas o menos la misma.
             </Text>
             <TitleText extraSize style={styles.title}>
                ¿Es fácil que se genere una cita grupal?
@@ -60,9 +41,62 @@ const AboutPage: FC = () => {
             <EmptySpace height={15} />
             <Text style={styles.text}>
                Algo que ayuda es que en la lógica de la monogamia te ves con una sola persona por lo
-               que tiene que "tenerlo todo" pero aquí hay multiples personas por lo que no hace
-               falta que nadie lo tenga todo, se dan relaciones más flexibles y relajadas en ese
-               sentido, generando más likes y coincidencias.
+               que esta tiene que "tenerlo todo" pero en las citas de esta app hay multiples
+               personas por lo que se puede evaluar perfiles con una mentalidad más relajada y
+               abierta, generando más likes y coincidencias.
+            </Text>
+            <TitleText extraSize style={styles.title}>
+               ¿Se pueden generar citas desequilibradas? (Ej: 1 persona se gusta con 5 que no se
+               gustan entre ellas)
+            </TitleText>
+            <Text style={styles.text}>
+               No. Solo se generan grupos más o menos equilibrados. Esto también genera que en las
+               citas 100% heterosexuales la cantidad de mujeres y hombres siempre va a ser mas o
+               menos la misma.
+            </Text>
+            <TitleText extraSize style={styles.title}>
+               ¿Si solo me gusta un género, significa que en la cita grupal van a ser todxs de ese
+               género?
+            </TitleText>
+            <Text style={styles.text}>
+               No. Para estar en una cita grupal te tienes que gustar con un mínimo de 2 de sus
+               integrantes, no necesariamente con la totalidad, es probable que haya personas en tus
+               citas que pueden ser de cualquier género y sexualidad.
+            </Text>
+            <TitleText extraSize style={styles.title}>
+               ¿Hay diferencias importantes entre las relaciones grupales y las tradicionales de 2
+               personas?
+            </TitleText>
+            <Text style={styles.text}>
+               Si, las relaciones afectivas y sexuales entre varias personas son la manera original
+               en la que se relacionaba el ser humano y definen el concepto de familia. Hasta hace
+               10.000 años la crianza era una actividad poco exigente, los padres de lxs hijxs eran
+               todas las personas de un grupo afectivo, se compartía el cuidado y los recursos de
+               forma igualitaria como una gran familia, potenciado por el sexo que solía ser grupal
+               y muy abundante. Este pasado poliamoroso grupal lo tenemos todxs presente en la
+               sexualidad biológica de nuestro cuerpo, por ejemplo: Los "gemidos sexuales" son un
+               instinto que las otras especies que lo tienen (primates) lo usan para llamar a lxs
+               que están cerca, de la misma manera funcionaba en nuestra especie.
+            </Text>
+            <EmptySpace height={15} />
+            <Text style={styles.text}>
+               La sexualidad que podemos experimentar en nuestra cultura esta muy reprimida y
+               alejada del tipo de sexualidad que nos pide nuestro cuerpo y su evolución, este es el
+               trasfondo de muchos problemas que surgen hoy en día a la hora de mantenernos
+               motivados sexualmente y conectar con nuevas personas, algo similar suele suceder con
+               el afecto. Hay grandes libros de divulgación científica sobre el tema para
+               recomendar, algunos muy conocidos y comentados por la comunidad científica como los
+               de Cacilda Jethá y Christopher Ryan en primer lugar o también el de Friedrich Engels:
+               "El origen de la familia, la propiedad privada y el estado".
+            </Text>
+            <EmptySpace height={15} />
+            <Text style={styles.text}>
+               En el último 3% de la historia del humano inteligente aparece la monogamia debido a
+               necesidades comerciales: casar hijxs como contrato comercial entre familias, acumular
+               riqueza de forma individual, herencias, etc. La monogamia y la familia de hoy en día
+               (nuclear) son dos de los muchos fenómenos sociales de la revolución neolítica, la
+               misma que trajo por primera vez a las clases sociales, patriarcado, explotación y
+               jerarquías culturales.
             </Text>
             <TitleText extraSize style={styles.title}>
                ¿Hay que pagar algo?
@@ -72,42 +106,6 @@ const AboutPage: FC = () => {
                abierto. Financiada con donaciones a voluntad, el único fin es ayudar para que
                iniciarse en el poliamor sea mas fácil en la práctica y difundir la idea de que las
                citas pueden ser de mas de dos personas.
-            </Text>
-            <TitleText extraSize style={styles.title}>
-               ¿Hay diferencias importantes entre las relaciones grupales y las tradicionales de 2
-               personas?
-            </TitleText>
-            <Text style={styles.text}>
-               Las relaciones afectivas y sexuales entre varias personas son la manera original en
-               la que se relacionaba el ser humano y definen el concepto de familia. Hasta hace
-               10.000 años la crianza era una actividad poco exigente, los padres de lxs hijxs eran
-               todas las personas de un grupo afectivo, se compartía el cuidado y los recursos de
-               forma igualitaria como una gran familia, potenciado por el sexo que solía ser grupal
-               y muy abundante. Este pasado poliamoroso grupal lo tenemos todxs presente en la
-               sexualidad biológica de nuestro cuerpo, por ejemplo: Los "gemidos sexuales" son un
-               instinto que las otras especies que lo tienen (primates) lo usan para llamar a lxs
-               que están cerca, de la misma manera funcionaba antes en nuestra especie.
-            </Text>
-            <EmptySpace height={15} />
-            <Text style={styles.text}>
-               La sexualidad que podemos experimentar en nuestra cultura esta muy reprimida y
-               alejada del tipo de sexualidad que nos pide nuestro cuerpo y su evolución, este
-               alejamiento es el trasfondo de muchos problemas que surgen hoy en día a la hora de
-               mantenernos motivados o conectar sexualmente, algo similar suele suceder con el
-               afecto. Hay grandes libros de divulgación científica sobre el tema para recomendar,
-               algunos muy conocidos y comentados por la comunidad científica como los de Cacilda
-               Jethá y Christopher Ryan en primer lugar o también el de Friedrich Engels: "El origen
-               de la familia, la propiedad privada y el estado".
-            </Text>
-            <EmptySpace height={15} />
-            <Text style={styles.text}>
-               Cuando se instala la monogamia en nuestra cultura, no sucede por que esta tenga
-               alguna ventaja sentimental, aparece por necesidades comerciales: casar hijxs como
-               contrato comercial entre familias, acumular riqueza de forma personal sin compartir
-               colectivamente, herencia, etc. La monogamia y la familia de hoy en día (nuclear) son
-               dos de los muchos fenómenos sociales de la revolución neolítica, la misma que trajo
-               por primera vez a las clases sociales, patriarcado, explotación y otras formas de
-               opresión.
             </Text>
             <TitleText extraSize style={styles.title}>
                ¿Quienes hicieron esta aplicación?
