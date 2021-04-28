ea6cf155f9fcb54da83ae0a430bb24d117a5c913
Fernando Medina <fermmm@gmail.com>
2021-04-28 19:20:20 -0300

    Improved beta version message

--- common-tools/messages/showBetaVersionMessage.ts
+++ common-tools/messages/showBetaVersionMessage.ts
@@ -3,7 +3,7 @@ import { Alert } from "react-native";
 export function showBetaVersionMessage() {
    Alert.alert(
       "",
-      "Esta app acaba de ser terminada y es para pruebas. La vamos a difundir después de la pandemia. Puede que no encuentres a nadie por tu zona",
+      "Esta app acaba de ser terminada y es para pruebas. La vamos a difundir cuando termine el problema del COVID. Puede que no encuentres a nadie por tu zona",
       [
          {
             text: "Entendido"
