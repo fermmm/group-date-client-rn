1bd1a776730070b2f59590df30a404194b1fc207
Fernando Medina <fermmm@gmail.com>
2021-05-05 13:19:37 -0300

    Implemented token checker function for google login

--- api/authentication/providers/google/checkGoogleToken.ts
+++ api/authentication/providers/google/checkGoogleToken.ts
@@ -1,6 +1,11 @@
 import { httpRequest } from "../../../tools/httpRequest";
 
 export async function checkGoogleToken(token: string) {
-   // TODO: Implement
-   return { valid: true };
+   const response: { email: string } = await httpRequest({
+      baseURL: `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${token}`,
+      errorResponseSilent: true,
+      handleErrors: true
+   });
+
+   return { valid: response?.email != null };
 }
