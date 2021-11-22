6f737fa069a1b1d9189fe2d666619e07ac2737c0
Fernando Medina <fermmm@gmail.com>
2021-11-21 23:49:52 -0300

    Improved app update dialog

--- components/pages/LoginPage/AppUpdateMessage/AppUpdateMessage.tsx
+++ components/pages/LoginPage/AppUpdateMessage/AppUpdateMessage.tsx
@@ -1,17 +1,42 @@
 import React, { FC } from "react";
-import { StyleSheet } from "react-native";
+import { Linking, Platform, StyleSheet, View } from "react-native";
+import * as Application from "expo-application";
 import { Text } from "react-native-paper";
 import { ServerInfoResponse } from "../../../../api/server/shared-tools/endpoints-interfaces/server-info";
+import { useTheme } from "../../../../common-tools/themes/useTheme/useTheme";
 import { Styles } from "../../../../common-tools/ts-tools/Styles";
 import { currentTheme } from "../../../../config";
+import ButtonStyled from "../../../common/ButtonStyled/ButtonStyled";
 
 interface PropsAppUpdateMessage {
    serverInfo: ServerInfoResponse;
 }
 
 const AppUpdateMessage: FC<PropsAppUpdateMessage> = ({ serverInfo }) => {
+   const { colors } = useTheme();
+
    if (serverInfo?.buildVersionIsCompatible === false) {
-      return <Text style={styles.textBlock}>Debes actualizar la app en Google Play</Text>;
+      return (
+         <View style={styles.mainContainer}>
+            <Text style={styles.textBlock}>
+               Debes actualizar la app en {Platform.OS === "ios" ? "App Store" : "Google Play"}
+            </Text>
+            <ButtonStyled
+               color={colors.textLogin}
+               style={styles.button}
+               contentStyle={styles.buttonContent}
+               onPress={() => {
+                  if (Platform.OS === "ios") {
+                     // TODO: Implement for IOS
+                  } else {
+                     Linking.openURL(`market://details?id=${Application.applicationId}`);
+                  }
+               }}
+            >
+               Ir a {Platform.OS === "ios" ? "App Store" : "Google Play"}
+            </ButtonStyled>
+         </View>
+      );
    }
 
    if (serverInfo?.codeVersionIsCompatible === false) {
@@ -26,12 +51,23 @@ const AppUpdateMessage: FC<PropsAppUpdateMessage> = ({ serverInfo }) => {
 };
 
 const styles: Styles = StyleSheet.create({
+   mainContainer: {
+      width: "100%"
+   },
    textBlock: {
       textAlign: "center",
       fontFamily: currentTheme.font.medium,
       color: currentTheme.colors.textLogin,
       fontSize: 15,
-      marginBottom: 150
+      marginBottom: 40
+   },
+   button: {
+      borderColor: currentTheme.colors.textLogin,
+      width: "100%",
+      marginBottom: 80
+   },
+   buttonContent: {
+      justifyContent: "center"
    }
 });
 
