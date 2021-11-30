98859205340717c4679884cf387486ac9fb8b5a3
Fernando Medina <fermmm@gmail.com>
2021-11-29 22:52:18 -0300

    Incremented version to: 1.1.8

--- app.config.ts
+++ app.config.ts
@@ -15,17 +15,17 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
       resizeMode: "cover",
       backgroundColor: "#000000"
    },
-   version: "1.1.7",
+   version: "1.1.8",
    android: {
       package: "com.poly.dates",
-      versionCode: 117,
+      versionCode: 118,
       useNextNotificationsApi: true,
       permissions: ["CAMERA", "CAMERA_ROLL", "ACCESS_COARSE_LOCATION", "ACCESS_FINE_LOCATION"],
       googleServicesFile: "./android/app/google-services.json"
    },
    ios: {
       bundleIdentifier: "com.poly.dates",
-      buildNumber: "1.1.7",
+      buildNumber: "1.1.8",
       supportsTablet: false,
       infoPlist: {
          NSCameraUsageDescription:
