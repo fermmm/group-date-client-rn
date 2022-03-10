1b5bcd92bf88052a4ee7ebaac39dba2e336e6ffa
Fernando <fermmm@gmail.com>
2022-03-10 04:21:46 -0300

    Improved security text

--- components/pages/AboutPage/AboutPage.tsx
+++ components/pages/AboutPage/AboutPage.tsx
@@ -53,20 +53,20 @@ const AboutPage: FC = () => {
                ¿Es segura una cita grupal para las mujeres?
             </TitleText>
             <Text style={styles.text}>
-               Si, es incluso mas segura que las apps de monogamia ya que en las citas grupales que
-               la app genera va a haber mas de una mujer dando la posibilidad de cuidarse mutuamente
-               igual que en cualquier salida grupal. No tenemos ninguna queja sobre seguridad hasta
-               ahora. Por si acaso incluimos un mensaje de advertencia para las mujeres indicando
-               que cuando asistan a una cita primero se encuentren entre ellas y luego se conozcan
-               todxs en un lugar público y seguro (como en cualquier app de este tipo). También
-               tenemos todos los sistemas de seguridad de las apps de este tipo como el botón de la
-               bandera (flag) junto al de like para denunciar usuarios sospechosos.
+               Si, es mucho más segura que las apps de monogamia ya que en las citas grupales de
+               esta app hay más de una mujer pudiendo cuidarse mutuamente igual que en cualquier
+               salida grupal. No tenemos ninguna queja sobre seguridad hasta ahora. Por si acaso
+               también incluimos un mensaje para las mujeres recomendando que cuando asistan a una
+               cita se encuentren antes entre mujeres y todo sea en un lugar público y seguro (como
+               en cualquier app de este tipo). También tenemos los mismos sistemas que las otras
+               apps para denunciar.
             </Text>
             <Text style={styles.text}>
-               Por último como una simple opinión nuestra, pensamos que a las personas irrespetuosas
-               o violentas les interesan prácticas donde pueden ejercer jerarquía como en la
-               monogamia, múltiples parejas o agregar gente a la pareja, esta app propone y apoya
-               solo citas grupales no-jerárquicas (nos conocemos todxs a la vez).
+               Por último les compartimos una opinión: pensamos que a las personas irrespetuosas o
+               violentas no les interesa esta app, les interesan formas de relación donde pueden
+               ejercer jerarquía fácilmente como en las parejas, multiples parejas o agregando una
+               persona a su pareja, esta app solo soporta citas grupales no-jerárquicas (conocernos
+               a la vez sin centralismos), más info en pregunta anterior.
             </Text>
             <TitleText extraSize style={styles.title}>
                ¿En la cita grupal van a ser todxs del género y sexualidad que me gusta?
