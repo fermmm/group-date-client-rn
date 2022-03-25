10946fe48423a6fa95eff536741ddbed848f0077
Fernando Medina <fermmm@gmail.com>
2022-03-24 22:08:18 -0300

    Restored welcome message

--- common-tools/messages/showBetaVersionMessage.ts
+++ common-tools/messages/showBetaVersionMessage.ts
@@ -6,13 +6,13 @@ export function showIntroMessage() {
       return;
    }
 
-   // Alert.alert(
-   //    "",
-   //    "Esta app acaba de ser terminada y es para pruebas. La vamos a difundir cuando termine el problema del COVID. Puede que no encuentres a nadie por tu zona",
-   //    [
-   //       {
-   //          text: "Entendido"
-   //       }
-   //    ]
-   // );
+   Alert.alert(
+      "",
+      "Esta app acaba de ser terminada y ahora estamos planificando la difusión. Puede que aún no encuentres a nadie por tu zona",
+      [
+         {
+            text: "Entendido"
+         }
+      ]
+   );
 }
