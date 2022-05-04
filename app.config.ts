88569340c87c50904e811ff73551ceaba4cd21a7
Fernando <fermmm@gmail.com>
2022-05-03 21:25:15 -0300

    Changed version to 1.4.2

--- app.config.ts
+++ app.config.ts
@@ -24,17 +24,17 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
       resizeMode: "cover",
       backgroundColor: "#000000"
    },
-   version: "1.4.1",
+   version: "1.4.2",
    android: {
       package: "com.poly.dates",
-      versionCode: 141,
+      versionCode: 142,
       useNextNotificationsApi: true,
       permissions: ["CAMERA", "CAMERA_ROLL", "ACCESS_COARSE_LOCATION", "ACCESS_FINE_LOCATION"],
       googleServicesFile: "./android/app/google-services.json"
    },
    ios: {
       bundleIdentifier: "com.poly.dates",
-      buildNumber: "1.4.1",
+      buildNumber: "1.4.2",
       supportsTablet: false,
       infoPlist: {
          CFBundleAllowMixedLocalizations: true,
