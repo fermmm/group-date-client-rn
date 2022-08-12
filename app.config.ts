f8554b2923c6b2247ba961eb4e2cf7cfb5e8c574
Fernando <fermmm@gmail.com>
2022-08-12 00:54:53 -0300

    Incremented version to 1.4.6

--- app.config.ts
+++ app.config.ts
@@ -24,17 +24,17 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
       resizeMode: "cover",
       backgroundColor: "#000000"
    },
-   version: "1.4.5",
+   version: "1.4.6",
    android: {
       package: "com.poly.dates",
-      versionCode: 145,
+      versionCode: 146,
       useNextNotificationsApi: true,
       permissions: ["CAMERA", "CAMERA_ROLL", "ACCESS_COARSE_LOCATION", "ACCESS_FINE_LOCATION"],
       googleServicesFile: "./android/app/google-services.json"
    },
    ios: {
       bundleIdentifier: "com.poly.dates",
-      buildNumber: "1.4.5",
+      buildNumber: "1.4.6",
       supportsTablet: false,
       infoPlist: {
          CFBundleAllowMixedLocalizations: true,
